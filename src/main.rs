85e30ec810a6a06f6f268c0c9391703a332b4397
Hartley McGuire <skipkayhil@gmail.com>
2022-05-23 02:51:38 -0400

    Make SdevCommand a wrapper for Command
    This simplifies the creation of Commands as there is less explicit
    string conversion, and kind of leads to way towards SdevCommand turning
    into an even lighter wrapper (providing print/run methods and that's
    it).

--- src/main.rs
+++ src/main.rs
@@ -33,56 +33,44 @@ enum Commands {
 }
 
 struct SdevCommand {
-    program: String,
-    args: Vec<String>,
+    command: Command,
 }
 
 impl SdevCommand {
     fn clone(repo: &Repo) -> Self {
-        Self {
-            program: "git".to_string(),
-            args: vec![
-                "clone".to_string(),
-                repo.to_url(),
-                repo.to_path_with_base(&env::var("HOME").expect("unknown HOME directory"))
-                    .display()
-                    .to_string(),
-            ],
-        }
-    }
-
-    fn tmux(repo: &Repo, command: &str) -> Self {
-        Self {
-            program: "tmux".to_string(),
-            args: vec![
-                command.to_string(),
-                "-t".to_string(),
-                repo.name().to_string(),
-            ],
-        }
-    }
-
-    fn run(&self) -> ExitStatus {
-        println_shell!("{}\n", &self);
-
-        let mut command = Command::new(&self.program);
-
-        for arg in self.args.iter() {
-            command.arg(&arg);
-        }
+        let mut command = Command::new("git");
 
         command
+            .arg("clone")
+            .arg(repo.to_url())
+            .arg(repo.to_path_with_base(&env::var("HOME").expect("unknown HOME directory")));
+
+        Self { command }
+    }
+
+    fn tmux(repo: &Repo, attach_command: &str) -> Self {
+        let mut command = Command::new("tmux");
+
+        command.arg(attach_command).arg("-t").arg(repo.name());
+
+        Self { command }
+    }
+
+    fn run(&mut self) -> ExitStatus {
+        println_shell!("{}\n", self);
+
+        self.command
             .status()
-            .unwrap_or_else(|_| panic!("failed to execute {}", &self.program))
+            .unwrap_or_else(|_| panic!("failed to execute {:?}", self.command.get_program()))
     }
 }
 
 impl fmt::Display for SdevCommand {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", &self.program)?;
+        write!(f, "{}", &self.command.get_program().to_str().unwrap())?;
 
-        for arg in self.args.iter() {
-            write!(f, " {}", arg)?;
+        for arg in self.command.get_args() {
+            write!(f, " {}", arg.to_str().unwrap())?;
         }
 
         Ok(())
