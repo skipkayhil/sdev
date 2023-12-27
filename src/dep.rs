534aec8e18444e613d63c36e9c08dd3dd475c6d8
Hartley McGuire <skipkayhil@gmail.com>
2023-12-27 15:44:37 -0500

    Fix Dep::process succeeding on false second met
    Previously, the return value of `::met` was returned as is from
    `::process`. While this was correct for cases where calling `::met` the
    second time would return an `Error`, it was not correct for cases where
    it would return `Status::Unmet`. In those cases, `::process` should
    actually have been returning an `Error` because `::met` returning false
    means the `::meet` did not actually work.
    
    This commit addresses the issue by changing the return type of
    `::process` to `MeetResult`. This really just removes the `Status` from
    the returned `Result`, so that there is no case where the `Result` can
    be `Ok` but contain a `Status::Unmet`. This also cleaned up a bit of the
    implementation of `::process` as well, since requirements would return
    `UNMET` if their `::process` was `Unmet` when that case should also have
    returned an `Error`.
    
    Additionally, some very simple tests were added to exercise some of
    these flows through `::process`. The mock structs ended up with a lot of
    duplication but I haven't yet found a good way to implement defaults for
    met/meet only inside the test config.

--- src/dep.rs
+++ src/dep.rs
@@ -7,7 +7,9 @@ type MetResult = Result<Status, Unmeetable>;
 type MeetResult = Result<(), Unmeetable>;
 type Reqs = Vec<Box<dyn Dep>>;
 
+#[cfg(test)]
 const MET: MetResult = Ok(Status::Met);
+
 const UNMET: MetResult = Ok(Status::Unmet);
 
 pub enum Status {
@@ -20,6 +22,7 @@ impl Status {
         matches!(self, Status::Met)
     }
 
+    #[cfg(test)]
     fn is_unmet(&self) -> bool {
         matches!(self, Status::Unmet)
     }
@@ -61,6 +64,8 @@ mod status_tests {
 #[derive(thiserror::Error, Debug)]
 #[error("unmeetable")]
 pub enum Unmeetable {
+    #[error("dep is unmet after meet")]
+    UnmetAfterMeet,
     Shell(#[from] ShellError),
 }
 
@@ -76,25 +81,79 @@ pub trait Dep {
         vec![]
     }
 
-    fn process(&self) -> MetResult {
+    fn process(&self) -> MeetResult {
         for req in self.reqs_to_met().iter() {
-            if req.process()?.is_unmet() {
-                return UNMET;
-            }
+            req.process()?
         }
 
         if self.met()?.is_met() {
-            return MET;
+            return Ok(());
         }
 
         for req in self.reqs_to_meet().iter() {
-            if req.process()?.is_unmet() {
-                return UNMET;
-            }
+            req.process()?
         }
 
         self.meet()?;
 
-        self.met()
+        match self.met()? {
+            Status::Unmet => Err(Unmeetable::UnmetAfterMeet),
+            Status::Met => Ok(()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod dep_tests {
+    use super::*;
+
+    struct BlankDep;
+    impl Dep for BlankDep {
+        fn met(&self) -> MetResult {
+            MET
+        }
+
+        fn meet(&self) -> MeetResult {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn processable() {
+        assert!(BlankDep.process().is_ok());
+    }
+
+    struct RaisingMetDep;
+    impl Dep for RaisingMetDep {
+        fn met(&self) -> MetResult {
+            // UnmetAfterMeet won't really be raised here, but it's easier to mock than creating a
+            // nested ShellError and io::Error
+            Err(Unmeetable::UnmetAfterMeet)
+        }
+
+        fn meet(&self) -> MeetResult {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn handles_errors_in_met() {
+        assert!(RaisingMetDep.process().is_err());
+    }
+
+    struct NeverMetDep;
+    impl Dep for NeverMetDep {
+        fn met(&self) -> MetResult {
+            UNMET
+        }
+
+        fn meet(&self) -> MeetResult {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn errors_when_unmet_after_meet() {
+        assert!(NeverMetDep.process().is_err());
     }
 }
