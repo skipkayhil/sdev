0fa821b052a5eeca9fd204c735a74cacd75a8bb7
Hartley McGuire <skipkayhil@gmail.com>
2023-12-30 02:12:39 -0500

    Refactor fuzzy_select
    Reduce nesting and remove "return Err" and "Ok()"

--- src/cmd.rs
+++ src/cmd.rs
@@ -23,31 +23,31 @@ pub fn fuzzy_select<'a, T>(options: T) -> Result<Option<String>, FzfError>
 where
     T: IntoIterator<Item = &'a OsStr>,
 {
-    let mut process = Command::new("fzf-tmux")
+    let process = Command::new("fzf-tmux")
         .arg("-p")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .map_err(FzfError::Spawn)?;
 
-    match process.stdin {
-        Some(ref mut stdin) => {
-            for option in options.into_iter() {
-                stdin
-                    .write(option.as_encoded_bytes())
-                    .map_err(FzfError::Write)?;
-                writeln!(stdin).map_err(FzfError::Write)?;
-            }
+    {
+        let mut stdin = process.stdin.as_ref().ok_or(FzfError::Pipe)?;
+
+        for option in options.into_iter() {
+            stdin
+                .write(option.as_encoded_bytes())
+                .map_err(FzfError::Write)?;
+            writeln!(stdin).map_err(FzfError::Write)?;
         }
-        None => return Err(FzfError::Pipe),
     };
 
-    let output = process.wait_with_output().map_err(FzfError::Output)?;
-
-    let selected = output
-        .status
-        .success()
-        .then(|| String::from_utf8_lossy(&output.stdout).trim().to_string());
-
-    Ok(selected)
+    process
+        .wait_with_output()
+        .map_err(FzfError::Output)
+        .map(|output| {
+            output
+                .status
+                .success()
+                .then(|| String::from_utf8_lossy(&output.stdout).trim().to_string())
+        })
 }
