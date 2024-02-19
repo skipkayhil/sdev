f34883e556ef764e59eab989debf9a80ac075698
Hartley McGuire <skipkayhil@gmail.com>
2024-02-19 16:20:13 -0500

    Use nucleo for fuzzy matching in z
    - setup the Nucleo instance
    - push all items/strings into Nucleo. For now, we'll push array indexes
      into the injector and then lookup strings from there. In the future
      this could be upgraded to some struct if desired
    
    - inside the loop we now only render matched paths from Nucleo
    
    - additionally, changes to the search now also have to reparse the
      search. The last param is an optimization where we tell Nucleo that
      the new search is an extension of the last one. Since the push/pop
      events are separated, they cleanly map to true/false.

--- src/cmd/z.rs
+++ src/cmd/z.rs
@@ -3,26 +3,43 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
+use nucleo::{
+    pattern::{CaseMatching, Normalization},
+    Config, Nucleo,
+};
 use ratatui::{
     layout::{Constraint, Layout},
     prelude::{CrosstermBackend, Line, Style, Stylize, Terminal},
     widgets::{Block, Borders, List, ListDirection, ListState},
 };
 use std::io::stdout;
+use std::sync::Arc;
 
 use crate::repositories::git_repos::{CachingRepository, FileSystemRepository, Repository};
-use crate::Config;
 
 const CHEVRON: &str = ">";
 
-pub fn run(config: Config) -> anyhow::Result<()> {
+pub fn run(config: crate::Config) -> anyhow::Result<()> {
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
 
+    let mut nucleo = Nucleo::<usize>::new(Config::DEFAULT, Arc::new(|| {}), None, 1);
+    let injector = nucleo.injector();
+
     let mut repository = CachingRepository::new(FileSystemRepository::new(config.root));
     let all_paths = repository.fetch_all()?;
+    // TODO: unwrap because paths should all be utf8... but it feels like a lib may help
+    // here
+    let path_strings: Vec<&str> = all_paths
+        .iter()
+        .map(|repo| repo.path().to_str().unwrap())
+        .collect();
+
+    for (i, path) in path_strings.iter().enumerate() {
+        injector.push(i, |dst| dst[0] = path.to_string().into());
+    }
 
     let mut search = String::new();
     let mut state = ListState::default().with_selected(Some(0));
@@ -30,17 +47,19 @@ pub fn run(config: Config) -> anyhow::Result<()> {
     let padded_chevron = format!("{CHEVRON} ");
 
     loop {
+        let _status = nucleo.tick(10);
+        let snap = nucleo.snapshot();
+
         terminal.draw(|frame| {
             let layout = Layout::vertical([Constraint::Percentage(100), Constraint::Min(1)])
                 .split(frame.size());
 
-            // TODO: unwrap because paths should all be utf8... but it feels like a lib may help
-            // here
-            let path_strings: Vec<&str> = all_paths
-                .iter()
-                .map(|repo| repo.path().to_str().unwrap())
+            let matched_paths: Vec<&str> = snap
+                .matched_items(0..snap.matched_item_count().min(layout[0].height.into()))
+                .map(|item| path_strings[*item.data])
                 .collect();
-            let path_list = List::new(path_strings)
+
+            let path_list = List::new(matched_paths)
                 .block(
                     Block::default()
                         .borders(Borders::BOTTOM)
@@ -68,9 +87,25 @@ pub fn run(config: Config) -> anyhow::Result<()> {
                 if key.kind == KeyEventKind::Press {
                     match key.code {
                         KeyCode::Esc => break,
-                        KeyCode::Char(key) => search.push(key),
+                        KeyCode::Char(key) => {
+                            search.push(key);
+                            nucleo.pattern.reparse(
+                                0,
+                                &search,
+                                CaseMatching::Smart,
+                                Normalization::Smart,
+                                true,
+                            );
+                        }
                         KeyCode::Backspace => {
                             search.pop();
+                            nucleo.pattern.reparse(
+                                0,
+                                &search,
+                                CaseMatching::Smart,
+                                Normalization::Smart,
+                                false,
+                            );
                         }
                         KeyCode::Up => match state.selected() {
                             None => state.select(Some(0)),
