b33e97f1761d8a2f94f6b8e453f114b044160904
Hartley McGuire <skipkayhil@gmail.com>
2024-08-10 18:01:38 -0400

    Replace clone println with ratatui
    This provides a bit better interactivity by updating the displayed text
    as steps finish. This also sets up a ratatui/Progress future where the
    fetch progress can be visualized.

--- src/dep/git.rs
+++ src/dep/git.rs
@@ -1,5 +1,4 @@
 use crate::dep::{Dep, MeetResult, MetResult};
-use crate::shell::{FG_GRAY, RESET};
 
 use std::fs;
 use std::path::PathBuf;
@@ -7,6 +6,14 @@ use std::path::PathBuf;
 use gix::interrupt::IS_INTERRUPTED;
 use gix::progress::Discard;
 use gix::Url;
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm,
+    prelude::{Line, Stylize, Widget},
+    style::Color,
+    widgets::Paragraph,
+    Terminal, TerminalOptions, Viewport,
+};
 
 pub struct Clone {
     url: Url,
@@ -25,23 +32,57 @@ impl Dep for Clone {
     }
 
     fn meet(&self) -> MeetResult {
-        println!("{}src{} {}", FG_GRAY, RESET, self.url);
-        println!("{}dst{} {}", FG_GRAY, RESET, self.path.to_string_lossy());
+        crossterm::terminal::enable_raw_mode()?;
+
+        let mut terminal = Terminal::with_options(
+            CrosstermBackend::new(std::io::stdout()),
+            TerminalOptions {
+                viewport: Viewport::Inline(5),
+            },
+        )?;
+
+        terminal.insert_before(1, |buf| {
+            Line::from(vec!["src".dark_gray(), format!(" {}", &self.url).into()])
+                .render(buf.area, buf);
+        })?;
+        terminal.insert_before(1, |buf| {
+            Line::from(vec![
+                "dst".dark_gray(),
+                format!(" {}", &self.path.to_string_lossy()).into(),
+            ])
+            .render(buf.area, buf);
+        })?;
 
         fs::create_dir_all(&self.path)?;
 
         let mut prepare_clone = gix::prepare_clone(self.url.clone(), &self.path)?;
 
-        println!("cloning...");
+        terminal.draw(|f| {
+            f.render_widget("cloning...", f.area());
+        })?;
 
         let (mut prepare_checkout, _) =
             prepare_clone.fetch_then_checkout(Discard, &IS_INTERRUPTED)?;
 
-        println!("checking out...");
+        terminal.insert_before(1, |buf| {
+            Line::from(vec!["✓".fg(Color::Indexed(42)), " fetched".into()]).render(buf.area, buf);
+        })?;
+
+        terminal.draw(|f| {
+            f.render_widget("checking out...", f.area());
+        })?;
 
         prepare_checkout.main_worktree(Discard, &IS_INTERRUPTED)?;
 
-        println!("done!");
+        terminal.insert_before(1, |buf| {
+            Paragraph::new(Line::from(vec![
+                "✓".fg(Color::Indexed(42)),
+                " cloned".into(),
+            ]))
+            .render(buf.area, buf);
+        })?;
+
+        crossterm::terminal::disable_raw_mode()?;
 
         Ok(())
     }
